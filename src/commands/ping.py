d49fb499f34d325aaf8b98f5e08d899a02a3e417
Qteix <55319061+Qteix@users.noreply.github.com>
2021-07-28 19:34:50 +0900

    Update ping.py

--- src/commands/ping.py
+++ src/commands/ping.py
@@ -1,4 +1,4 @@
-import time
+import time, psutil
 
 import vkquick as vq
 
@@ -9,4 +9,7 @@ from src.misc import app
 @app.command("пинг", invalid_argument_config=ErrorHandler())
 async def pinged(ctx: vq.NewMessage):
     delta = round(time.time() - ctx.msg.date.timestamp(), 4)
-    return f"✨ Ответ за ➙ {delta} сек."
+    return( f"✨ vqlp | Ответ за ➙ {delta} сек.\n"
+           f"⚙ Занятость ОЗУ : {round(psutil.virtual_memory()[3] / 2. ** 30, 2)}GB из {round(psutil.virtual_memory()[0] / 2. ** 30, 2)}GB\n"
+           f" 📈 Занятость ЦПУ {round(psutil.cpu_percent())} %\n "
+           f"📀 Диск загружен на {(psutil.disk_usage('/')[3])}%")
